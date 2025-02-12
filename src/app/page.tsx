36532186c19331f0bf1faf7005a416786787cb13
Cuong Dinh <huycuong.dinh@gmail.com>
2025-02-12 14:32:38 -0500

    Fix TypeScript issue with the dynamic import type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,17 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-const TranscriptionApp = dynamic(() => import('../components/TranscriptionApp'), {
-  ssr: false,
-  loading: () => (
-    <div className="min-h-screen flex items-center justify-center">
-      <p>Loading...</p>
-    </div>
-  ),
-});
+const TranscriptionApp = dynamic(
+  () => import('../components/TranscriptionApp').then(mod => mod.default),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="min-h-screen flex items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    ),
+  }
+);
 
 export default function Page() {
   return (
